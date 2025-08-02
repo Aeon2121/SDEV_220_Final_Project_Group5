3018af1b920bc2a970eb38a1be735892a8d1335d
jrdostal <dostal325@gmail.com>
2025-08-02 12:09:24 -0400

    updating based on sales module changes

--- Project_Final/customer_module/models.py
+++ Project_Final/customer_module/models.py
@@ -1,45 +1,19 @@
-'''from django.conf import settings
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-
-from inventory_module.models import Inventory
-from online_ordering import Customer
-
-class Order(models.Model):
-    Customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null = False)
-    Order_Number = models.IntegerField(unique = True, null = False)
-    Order_Info = models.CharField(max_length=200, null = False)
-    Order_date = models.DateTimeField(blank=True, null=True)
-
-    def set_customer(self):
-        self.Customer = Customer.__name__
-
-    def set_order_number(self):
-        self.Order_Number = Inventory.Order_Number
-
-    def set_order_info(self):
-        self.Order_Info = Inventory.Order_info
-
-    def publish(self):
-        self.order_date = timezone.now()
-        self.save()
-
-    def __init__(self):
-        self.set_customer()
-        self.set_order_info()
-        self.set_order_number()
-        '''
-
-
+from unittest.util import _MAX_LENGTH
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from inventory_module.models import Inventory
 
+'''
+from ...online_ordering import Customer 
+from ...online_ordering import Order as online_order #v1
+
+from sales_module import OrderSys1 #v2
+'''
+
 class Order(models.Model):
     customer = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='orders'
     )
@@ -51,10 +25,27 @@ class Order(models.Model):
     order_number = models.IntegerField(unique=True)
     order_info = models.CharField(max_length=200)
     order_date = models.DateTimeField(blank=True, null=True)
+    customer_name = models.CharField(max_length=100)
 
     def publish(self):
         self.order_date = timezone.now()
         self.save()
 
+    '''
+    def set_customer_name(self):
+        self.customer_name = Customer.__name__
+
+    def set_order_number(self):
+        self.Order_Number = online_order.order_counter
+
+    def set_order_info(self):
+        self.Order_Info = str(online_order.items + online_order.status) #v1
+        self.Order_Info = OrderSys1.orders[self.order_number] #v2
+    def __init__(self):
+        self.set_order_info()
+        self.set_order_number()
+        self.set_customer_name()
+    '''
+        
     def __str__(self):
         return f"Order #{self.order_number} by {self.customer}"
