37090b54e553a3717ba2ce333a422a688af5d40c
jrdostal <dostal325@gmail.com>
2025-08-02 12:40:41 -0400

    updated order class attributes

--- Project_Final/customer_module/models.py
+++ Project_Final/customer_module/models.py
@@ -3,12 +3,7 @@ from django.db import models
 from django.utils import timezone
 from inventory_module.models import Inventory
 
-'''
-from ...online_ordering import Customer 
-from ...online_ordering import Order as online_order #v1
-
-from sales_module import OrderSys1 #v2
-'''
+from sales_module import OrderSys1
 
 class Order(models.Model):
     customer = models.ForeignKey(
@@ -30,21 +25,15 @@ class Order(models.Model):
         self.order_date = timezone.now()
         self.save()
 
-    '''
-    def set_customer_name(self):
-        self.customer_name = Customer.__name__
-
     def set_order_number(self):
-        self.Order_Number = online_order.order_counter
+        self.Order_Number = OrderSys1.order_counter
 
     def set_order_info(self):
-        self.Order_Info = str(online_order.items + online_order.status) #v1
-        self.Order_Info = OrderSys1.orders[self.order_number] #v2
+        self.Order_Info = OrderSys1.orders[self.order_number]
+
     def __init__(self):
         self.set_order_info()
         self.set_order_number()
-        self.set_customer_name()
-    '''
-        
+       
     def __str__(self):
         return f"Order #{self.order_number} by {self.customer}"
